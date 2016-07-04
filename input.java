346e4388fa863e1522f8338c2e38914cb78665dc
Tomi De Lucca <tomasdelucca@gmail.com>
2016-07-03 21:37:46 -0300

    removed java files

--- input.java
+++
@@ -1,7 +0,0 @@
-import java.util.Scanner;public class input {
-private static Scanner scanner = new Scanner(System.in);public static void main(String args[]) { 
-String input = "";
-input = scanner.nextLine();
-System.out.print(input);
-}
-}
